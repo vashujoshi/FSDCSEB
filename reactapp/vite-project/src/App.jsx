19eb1f4bf219ab10d4691471cdf1552452bc05cb
vashujoshi <118829508+vashujoshi@users.noreply.github.com>
2024-11-14 12:09:07 +0530

    added new

--- reactapp/vite-project/src/App.jsx
+++ reactapp/vite-project/src/App.jsx
@@ -1,22 +1,40 @@
-import React from "react";
-import Student from "./student";
-
-function App() 
+import React from 'react'
+import Student from './student';
+function App()
 {
+  const h1=<h1>hello world</h1>;
   const mystyle={
-    color:"red",backgroundColor:'yellow'
+    color:'red',
+    backgroundColor:'yellow'
   }
-  
   return (
-    <div style={{backgroundColor:'cyan'}}>
+    <div style={{backgroundColor:'blueviolet'}}>
       {h1}
-      <div style={mystyle}>
-        Abes Engineering College
+      <div style={mystyle}> 
+        ABes Engineering college
       </div>
-      
-      <Student college="Abes Engineering College"
-      name=""/>
+      <div style={{display:'flex'}}>
+        <Student college="ABES ENGINEERING COLLEGE"
+        name="harshit"
+        branch="cse"
+        Roll="12"
+        section="a" 
+        pic={<img src='https://commondatastorage.googleapis.com/codeskulptor-demos/riceracer_assets/img/srixner.png' height={100} width={100}/>}
+       />
+
+        <Student college="ABES ENGINEERING COLLEGE"
+        name="verma"
+        branch="cse"
+        Roll="13"
+        section="a" />
+         <Student college="ABES ENGINEERING COLLEGE"
+        name="verma"
+        branch="cse"
+        Roll="13"
+        section="a" />
+
+      </div>
     </div>
   )
 }
-export default App;
+export default App
--- reactapp/vite-project/src/student.jsx
+++ reactapp/vite-project/src/student.jsx
@@ -1,16 +1,22 @@
-import React from "react";
+import React from 'react'
 
-function Student(props){
-    return(
-        <div className='icard'>
-            <table>
-                <tbody>
-                    <tr><td>College: {props.college}</td></tr>
-                    <tr><td>Name: {props.name}</td></tr>
-                    <tr><td colSpan={2}>ABES Engineering College</td></tr>
-                </tbody>    
-            </table>
-        </div>
-    )
+
+function Student(props) {
+  return (
+    <div className='icard'>
+        <table>
+            <tbody>
+                <tr><td colSpan={2}>{props.college}</td></tr>
+                <tr><td>Namw</td><td>{props.name}</td></tr>
+                <tr><td>    
+                    </td><td>{props.pic}</td></tr>
+                <tr><td>Branch</td><td>{props.branch}</td></tr>
+                <tr><td>roll</td><td>{props.Roll}</td></tr>
+                <tr><td>Section</td><td>{props.section}</td></tr>
+
+            </tbody>
+        </table>
+    </div>
+  )
 }
-export default Student;
+export default Student
